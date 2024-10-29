9179a23f5dc6d35704f3f6e3b534d9dcf2a37245
Facundo Tuesca <facundo.tuesca@trailofbits.com>
2024-10-29 15:09:49 +0100

    Get provenance URL using the simple API
    Signed-off-by: Facundo Tuesca <facundo.tuesca@trailofbits.com>

--- src/pip_plugin_pep740/_impl.py
+++ src/pip_plugin_pep740/_impl.py
@@ -22,6 +22,49 @@ if TYPE_CHECKING:
 PluginType = Literal["dist-inspector"]
 
 
+def _get_provenance_url(filename: str, index_host: str) -> str | None:
+    if filename.endswith(".tar.gz"):
+        name, _ = parse_sdist_filename(filename)
+    elif filename.endswith(".whl"):
+        name, _, _, _ = parse_wheel_filename(filename)
+    else:
+        # Unexpected file, ignore
+        return None
+
+    simple_index_package_url = (
+        builder.URIBuilder()
+        .add_scheme("https")
+        .add_host(index_host)
+        .add_path(f"simple/{name}/")
+        .geturl()
+    )
+    try:
+        r = requests.get(
+            url=simple_index_package_url,
+            headers={"Accept": "application/vnd.pypi.simple.v1+json"},
+            timeout=5,
+        )
+        r.raise_for_status()
+    except requests.RequestException as e:
+        msg = f"Error accessing PyPI simple API: {e}"
+        raise ValueError(msg) from e
+
+    try:
+        package_json = r.json()
+    except JSONDecodeError as e:
+        msg = f"Invalid PyPI simple index JSON response: {e}"
+        raise ValueError(msg) from e
+
+    matching_artifacts = [f for f in package_json["files"] if f["filename"] == filename]
+    if len(matching_artifacts) == 0:
+        msg = f"Could not find file {filename} using the simple API at {index_host}"
+        raise ValueError(msg)
+
+    artifact_info = matching_artifacts[0]
+    provenance_url: str | None = artifact_info.get("provenance")
+    return provenance_url
+
+
 def _get_provenance(filename: str, url: str) -> Provenance | None:
     """Download the provenance for a given distribution."""
     url_authority = rfc3986.api.uri_reference(url).authority
@@ -33,44 +76,28 @@ def _get_provenance(filename: str, url: str) -> Provenance | None:
     else:
         return None
 
-    if filename.endswith(".tar.gz"):
-        name, version = parse_sdist_filename(filename)
-    elif filename.endswith(".whl"):
-        name, version, _, _ = parse_wheel_filename(filename)
-    else:
-        # Unexpected file, ignore
+    provenance_url = _get_provenance_url(filename=filename, index_host=index_host)
+    if provenance_url is None:
+        # Can't verify artifacts uploaded without attestations
         return None
-
-    provenance_url = (
-        builder.URIBuilder()
-        .add_scheme("https")
-        .add_host(index_host)
-        .add_path(f"integrity/{name}/{version}/{filename}/provenance")
-        .geturl()
-    )
     try:
         r = requests.get(
             url=provenance_url,
-            params={"Accept": "application/vnd.pypi.integrity.v1+json"},
+            headers={"Accept": "application/vnd.pypi.integrity.v1+json"},
             timeout=5,
         )
         r.raise_for_status()
-    except requests.HTTPError as e:
-        # If there is no provenance available, continue
-        if e.response.status_code == requests.codes.not_found:
-            return None
-        raise ValueError(e) from e
     except requests.RequestException as e:
         msg = f"Error downloading provenance file: {e}"
         raise ValueError(msg) from e
 
     try:
         return Provenance.model_validate(r.json())
-    except ValidationError as e:
-        msg = f"Invalid provenance: {e}"
-        raise ValueError(msg) from e
     except JSONDecodeError as e:
         msg = f"Invalid provenance JSON: {e}"
+        raise ValueError(msg) from e
+    except ValidationError as e:
+        msg = f"Invalid provenance: {e}"
         raise ValueError(msg) from e
 
 
