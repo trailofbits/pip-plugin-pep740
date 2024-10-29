88688a897252ba1854e76dbc4c19b4a786529207
Facundo Tuesca <facundo.tuesca@trailofbits.com>
2024-10-29 15:09:49 +0100

    Get provenance URL using the simple API
    Signed-off-by: Facundo Tuesca <facundo.tuesca@trailofbits.com>

--- test/test_impl.py
+++ test/test_impl.py
@@ -11,15 +11,12 @@ from pypi_attestations import Distribution
 import pip_plugin_pep740
 
 PACKAGE_NAME = "abi3info"
-PACKAGE_VERSION_1 = "2024.10.8"
 DIST_FILE_1 = Path("test/assets/abi3info-2024.10.8-py3-none-any.whl")
 PROVENANCE_FILE_1 = Path("test/assets/abi3info-2024.10.8-py3-none-any.whl.provenance")
 
-PACKAGE_VERSION_2 = "2024.10.3"
 DIST_FILE_2 = Path("test/assets/abi3info-2024.10.3-py3-none-any.whl")
 PROVENANCE_FILE_2 = Path("test/assets/abi3info-2024.10.3-py3-none-any.whl.provenance")
 
-PACKAGE_VERSION_3 = "2024.10.8"
 DIST_FILE_3 = Path("test/assets/abi3info-2024.10.8.tar.gz")
 PROVENANCE_FILE_3 = Path("test/assets/abi3info-2024.10.8.tar.gz.provenance")
 
@@ -38,18 +35,26 @@ class TestPlugin:
         assert pip_plugin_pep740.plugin_type() == "dist-inspector"
 
     @pytest.mark.parametrize(
-        ("version", "filename", "provenance_file", "digest"),
+        ("filename", "provenance_file", "digest"),
         [
-            (PACKAGE_VERSION_1, DIST_FILE_1.name, PROVENANCE_FILE_1, DIST_DIGEST_1),
-            (PACKAGE_VERSION_3, DIST_FILE_3.name, PROVENANCE_FILE_3, DIST_DIGEST_3),
+            (DIST_FILE_1.name, PROVENANCE_FILE_1, DIST_DIGEST_1),
+            (DIST_FILE_3.name, PROVENANCE_FILE_3, DIST_DIGEST_3),
         ],
     )
     def test_pre_download_valid_provenance(
-        self, version: str, filename: str, provenance_file: Path, digest: str
+        self, filename: str, provenance_file: Path, digest: str
     ) -> None:
         with requests_mock.Mocker(real_http=True) as m:
             m.get(
-                f"https://pypi.org/integrity/{PACKAGE_NAME}/{version}/{filename}/provenance",
+                f"https://test.pypi.org/simple/{PACKAGE_NAME}/",
+                text=f'{{"files": [{{"filename": "{filename}", "provenance": "https://provenance_url"}}]}}',
+            )
+            m.get(
+                f"https://pypi.org/simple/{PACKAGE_NAME}/",
+                text=f'{{"files": [{{"filename": "{filename}", "provenance": "https://provenance_url"}}]}}',
+            )
+            m.get(
+                "https://provenance_url",
                 text=provenance_file.read_text(),
             )
             pip_plugin_pep740.pre_download(
@@ -77,8 +82,8 @@ class TestPlugin:
     def test_pre_download_no_provenance_found(self) -> None:
         with requests_mock.Mocker(real_http=True) as m:
             m.get(
-                f"https://pypi.org/integrity/{PACKAGE_NAME}/{PACKAGE_VERSION_1}/{DIST_FILE_1.name}/provenance",
-                status_code=404,
+                f"https://pypi.org/simple/{PACKAGE_NAME}/",
+                text=f'{{"files": [{{"filename": "{DIST_FILE_1.name}"}}]}}',
             )
             assert (
                 pip_plugin_pep740.pre_download(
@@ -89,20 +94,47 @@ class TestPlugin:
                 is None
             )
 
-    def test_pre_download_provenance_download_error(self) -> None:
+    def test_pre_download_index_http_error(self) -> None:
         with requests_mock.Mocker(real_http=True) as m:
             m.get(
-                f"https://pypi.org/integrity/{PACKAGE_NAME}/{PACKAGE_VERSION_1}/{DIST_FILE_1.name}/provenance",
+                f"https://pypi.org/simple/{PACKAGE_NAME}/",
                 status_code=403,
             )
             with pytest.raises(ValueError, match="403 Client Error"):
-                assert (
-                    pip_plugin_pep740.pre_download(
-                        url="https://files.pythonhosted.org/some_path",
-                        filename=DIST_FILE_1.name,
-                        digest=DIST_DIGEST_1,
-                    )
-                    is None
+                pip_plugin_pep740.pre_download(
+                    url="https://files.pythonhosted.org/some_path",
+                    filename=DIST_FILE_1.name,
+                    digest=DIST_DIGEST_1,
+                )
+
+    def test_pre_download_index_timeout(self) -> None:
+        with requests_mock.Mocker(real_http=True) as m:
+            m.get(
+                f"https://pypi.org/simple/{PACKAGE_NAME}/",
+                exc=requests.exceptions.ConnectTimeout,
+            )
+            with pytest.raises(ValueError, match="Error accessing PyPI simple API"):
+                pip_plugin_pep740.pre_download(
+                    url="https://files.pythonhosted.org/some_path",
+                    filename=DIST_FILE_1.name,
+                    digest=DIST_DIGEST_1,
+                )
+
+    def test_pre_download_provenance_download_error(self) -> None:
+        with requests_mock.Mocker(real_http=True) as m:
+            m.get(
+                f"https://pypi.org/simple/{PACKAGE_NAME}/",
+                text=f'{{"files": [{{"filename": "{DIST_FILE_1.name}", "provenance": "https://provenance_url"}}]}}',
+            )
+            m.get(
+                "https://provenance_url",
+                status_code=403,
+            )
+            with pytest.raises(ValueError, match="403 Client Error"):
+                pip_plugin_pep740.pre_download(
+                    url="https://files.pythonhosted.org/some_path",
+                    filename=DIST_FILE_1.name,
+                    digest=DIST_DIGEST_1,
                 )
 
     def test_pre_download_not_pypi_url(self) -> None:
@@ -118,23 +150,28 @@ class TestPlugin:
     def test_pre_download_provenance_timeout(self) -> None:
         with requests_mock.Mocker(real_http=True) as m:
             m.get(
-                f"https://pypi.org/integrity/{PACKAGE_NAME}/{PACKAGE_VERSION_1}/{DIST_FILE_1.name}/provenance",
+                f"https://pypi.org/simple/{PACKAGE_NAME}/",
+                text=f'{{"files": [{{"filename": "{DIST_FILE_1.name}", "provenance": "https://provenance_url"}}]}}',
+            )
+            m.get(
+                "https://provenance_url",
                 exc=requests.exceptions.ConnectTimeout,
             )
             with pytest.raises(ValueError, match="Error downloading provenance file"):
-                assert (
-                    pip_plugin_pep740.pre_download(
-                        url="https://files.pythonhosted.org/some_path",
-                        filename=DIST_FILE_1.name,
-                        digest=DIST_DIGEST_1,
-                    )
-                    is None
+                pip_plugin_pep740.pre_download(
+                    url="https://files.pythonhosted.org/some_path",
+                    filename=DIST_FILE_1.name,
+                    digest=DIST_DIGEST_1,
                 )
 
     def test_pre_download_invalid_provenance(self) -> None:
         with requests_mock.Mocker(real_http=True) as m:
             m.get(
-                f"https://pypi.org/integrity/{PACKAGE_NAME}/{PACKAGE_VERSION_1}/{DIST_FILE_1.name}/provenance",
+                f"https://pypi.org/simple/{PACKAGE_NAME}/",
+                text=f'{{"files": [{{"filename": "{DIST_FILE_1.name}", "provenance": "https://provenance_url"}}]}}',
+            )
+            m.get(
+                "https://provenance_url",
                 text=PROVENANCE_FILE_2.read_text(),
             )
             with pytest.raises(
@@ -147,10 +184,43 @@ class TestPlugin:
                     digest=DIST_DIGEST_1,
                 )
 
+    def test_pre_download_invalid_index_json(self) -> None:
+        with requests_mock.Mocker(real_http=True) as m:
+            m.get(f"https://pypi.org/simple/{PACKAGE_NAME}/", text="invalidjson")
+            with pytest.raises(
+                ValueError,
+                match="Invalid PyPI simple index JSON response",
+            ):
+                pip_plugin_pep740.pre_download(
+                    url="https://files.pythonhosted.org/some_path",
+                    filename=DIST_FILE_1.name,
+                    digest=DIST_DIGEST_1,
+                )
+
+    def test_pre_download_missing_package_from_index_json(self) -> None:
+        with requests_mock.Mocker(real_http=True) as m:
+            m.get(
+                f"https://pypi.org/simple/{PACKAGE_NAME}/",
+                text=f'{{"files": [{{"filename": "{DIST_FILE_2.name}", "provenance": "https://provenance_url"}}]}}',
+            )
+            with pytest.raises(
+                ValueError,
+                match=f"Could not find file {DIST_FILE_1.name} using the simple API at pypi.org",
+            ):
+                pip_plugin_pep740.pre_download(
+                    url="https://files.pythonhosted.org/some_path",
+                    filename=DIST_FILE_1.name,
+                    digest=DIST_DIGEST_1,
+                )
+
     def test_pre_download_invalid_provenance_json(self) -> None:
         with requests_mock.Mocker(real_http=True) as m:
             m.get(
-                f"https://pypi.org/integrity/{PACKAGE_NAME}/{PACKAGE_VERSION_1}/{DIST_FILE_1.name}/provenance",
+                f"https://pypi.org/simple/{PACKAGE_NAME}/",
+                text=f'{{"files": [{{"filename": "{DIST_FILE_1.name}", "provenance": "https://provenance_url"}}]}}',
+            )
+            m.get(
+                "https://provenance_url",
                 text="invalidjson",
             )
             with pytest.raises(
@@ -168,7 +238,11 @@ class TestPlugin:
         provenance["attestation_bundles"] = "invalid"
         with requests_mock.Mocker(real_http=True) as m:
             m.get(
-                f"https://pypi.org/integrity/{PACKAGE_NAME}/{PACKAGE_VERSION_1}/{DIST_FILE_1.name}/provenance",
+                f"https://pypi.org/simple/{PACKAGE_NAME}/",
+                text=f'{{"files": [{{"filename": "{DIST_FILE_1.name}", "provenance": "https://provenance_url"}}]}}',
+            )
+            m.get(
+                "https://provenance_url",
                 text=json.dumps(provenance),
             )
             with pytest.raises(
